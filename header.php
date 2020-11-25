506e359fac81ce0f7c4c851ffdd9b105494359f9
cindyeme <https://github.com/cindyeme/BleautechWebsite.git>
2020-11-25 12:44:45 +0100

    more improvement on header

--- header.php
+++ header.php
@@ -21,11 +21,11 @@
     <div class="container-fluid">
 
       <div class="row justify-content-center">
-        
-        <a href="index.php" class="d_none  logo mr-auto"><img src="assets/img/logo.png" alt="logo" class="img-fluid col-md-12 m-auto"></a>
 
-        <div class="col-xl-9 d-flex align-items-center">
-          <a href="index.php" class="logo mr-auto"><img src="assets/img/logo.png" alt="logo" class="img-fluid"></a>
+        <a href="index.php" class="d_none logo mr-auto"><img src="assets/img/logo.png" alt="logo" class="img-fluid col-md-12 m-auto"></a>
+
+        <div class="col-xl-9 d-flex align-items-center clearfix">
+          <a href="index.php" class="logo d_logo mr-auto"><img src="assets/img/logo.png" alt="logo" class="img-fluid"></a>
 
           <nav class="nav-menu d-none d-lg-block">
             <ul>
@@ -39,8 +39,8 @@
             </ul>
           </nav><!-- .nav-menu -->
 
-          <a href="sign_up.php" class="get-started-btn scrollto">Sign Up</a>
-          <a href="#" class="get-started-btn scrollto" data-toggle="modal" data-target="#sign_in">Sign In</a>
+          <a href="sign_up.php" class="get-started-btn scrollto mt-3">Sign Up</a>
+          <a href="#" class="get-started-btn scrollto mt-3" data-toggle="modal" data-target="#sign_in">Sign In</a>
         </div>
       </div>
 
@@ -57,7 +57,7 @@
           <h1>Bettter Library use Experience With Lib-Cater</h1>
           <h2>Maintains the record of the library.</h2>
           <p class="text-light p-2">Sign up to have unlimited access to books.</p>
-          <div class="mb-5"><a href="sign_up.php" class="btn-get-started scrollto ">Sign Up</a></div>
+          <div style="margin-bottom:4.5rem;"><a href="sign_up.php" class="btn-get-started scrollto ">Sign Up</a></div>
         </div>
         <div class="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150">
           <img src="assets/booksImg/library/book_step.png" class="img-fluid animated h-100 w-100" alt="">
