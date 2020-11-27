6aeb78d1704e16636986a8ffce1c843c1422d1ec
Emerenini Cynthia Ngozi <55026867+cindyeme@users.noreply.github.com>
2020-11-27 16:15:49 +0100

    latest update

--- includes/logSub.php
+++ includes/logSub.php
@@ -9,29 +9,30 @@
             return $data;
         }
 
-        if(isset($_POST['submit'])){
-            $serial_number = checkData($_POST['usn']);
-            $email = checkData($_POST['email']);
-            $password = checkData($_POST['password']);
+        $serial_number = checkData($_POST['usn']);
+        $email = checkData($_POST['email']);
+        $password = checkData($_POST['password']);
 
 
-                $newPassword = sha1($password);
-        
-                $sql = "SELECT * FROM `users` WHERE `serial_number` = '$serial_number' AND `email` = '$email' AND `password` = '$newPassword' ";
+            $newPassword = sha1($password);
+
+            $sql = "SELECT * FROM `users` WHERE `serial_number` = '$serial_number' AND `email` = '$email' AND `password` = '$newPassword' ";
 
 
-                $res = mysqli_query($conn, $sql);
-                $row = mysqli_fetch_assoc($res);
+            $res = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($res);
 
-                if(mysqli_num_rows($res) > 0){
-                    session_start();
-                    $_SESSION['id'] = $row['id'];
-                    $_SESSION['serial_number'] = $row['serial_number'];
-                    header('location: ../user_index.php');
-                } else{
-                    $errorMsg = " Please--enter--correct--login--details!";
-                    header('location: ../index.php?error='.$errorMsg);
-                }
+            if(mysqli_num_rows($res) > 0){
+                echo 1;
+                session_start();
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['serial_number'] = $row['serial_number'];
+                // header('location: ../user_index.php');
+            } else{
+                echo 0;
+                $errorMsg = " Please--enter--correct--login--details!";
+                // header('location: ../index.php?error='.$errorMsg);
             }
+
 
 ?>
