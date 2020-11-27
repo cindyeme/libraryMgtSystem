33b4662589feaa8e5970bb5fc4adfa9461cd0b86
Emerenini Cynthia Ngozi <55026867+cindyeme@users.noreply.github.com>
2020-11-27 15:30:45 +0100

    modifications

--- includes/retrieveSub.php
+++ includes/retrieveSub.php
@@ -1,36 +1,28 @@
 <?php
     require_once('conn.php');
-
-    $error = array();
         
 
-    if(isset($_POST['submit'])){
         //listen to when a user clicks on the register button.
         $email = isset($_POST['email'])? trim($_POST['email']) : '';
 
-        if(empty($email)){
-            $error[]  =  urlencode('please email required.');
+        $sql = "SELECT * FROM users WHERE email = '$email'"; 
+        $result = mysqli_query($conn, $sql); 
+
+        // check if user exists
+            // check if the email already exists in the database.
+        if(mysqli_num_rows($result) > 0) {
+
+            // getting a person's id in order to change password i.e using the id to update a user's pwd
+            $row = mysqli_fetch_assoc($result);
+            $id = base64_encode($row['id']);
+            //header("location: ../newPassword.php?id=" . $id);
+
+            echo $id;
+
+        } else {
+            echo 0;
+            $email_not_found = 'Email--not--found!';
+            // header('location: ../retrieve_email.php?error=' . $email_not_found);
         }
-
-        if(empty($error)){
-            $sql = "SELECT * FROM users WHERE email = '$email'"; 
-            $result = mysqli_query($conn, $sql); 
-
-                // check if user exists
-                 // check if the email already exists in the database.
-                if(mysqli_num_rows($result) > 0) {
-                    // getting a person's id in order to change password i.e using the id to update a user's pwd
-                    $row = mysqli_fetch_assoc($result);
-                    $id = base64_encode($row['id']);
-                    header("location: ../newPassword.php?id=" . $id);
-
-                // $email_exist = "$email already exists"; // throws this error when user exists
-                
-                } else {
-                    $email_not_found = 'Email--not--found!';
-                    header('location: ../retrieve_email.php?error=' . $email_not_found);
-                }
-        }
-    }
 
 ?>
