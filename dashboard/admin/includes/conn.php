c217c453e525bc8055b27287a21585def33928bb
Emerenini Cynthia Ngozi <55026867+cindyeme@users.noreply.github.com>
2020-11-27 15:37:51 +0100

    server connection

--- dashboard/admin/includes/conn.php
+++ dashboard/admin/includes/conn.php
@@ -1,8 +1,21 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'libsystem');
+// 	$conn = new mysqli('localhost', 'root', '', 'libsystem');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+// 	if ($conn->connect_error) {
+// 	    die("Connection failed: " . $conn->connect_error);
+// 	}
+
+ define("DB_SERVER", "db4free.net:3306");
+    define("DB_USER", "emereninicynthia");
+    define("DB_PASSWORD", "emereninicynthia0853");
+	define("DB_NAME", "emereninicynthia");
+	
+	$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+	if($conn){
+		//echo "connected";
+	}else{
+		//echo "Not connected";
 	}
 	
 ?>
