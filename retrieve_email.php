44e51ded7caf68a82adfdc7c287376b43c41650d
Emerenini Cynthia Ngozi <55026867+cindyeme@users.noreply.github.com>
2020-11-27 15:29:36 +0100

    ajax validations

--- retrieve_email.php
+++ retrieve_email.php
@@ -21,42 +21,75 @@
     <div class="forgot_password" id="sign_up">
         <div class="container forgot_container">
             <div class="row">
-            <div class="col-lg-12">
-            <div class="main_content">
+                <div class="col-lg-12">
+                    <div class="main_content">
             <!--  header -->
-            <div class="heading">
-                <h4 class="title">Confirm Email</h4>
-            </div>
+                <div class="heading">
+                    <h4 class="title">Confirm Email</h4>
+                </div>
 
             <!--  body -->
-            <div class="sign_up_body">
-                <form method="POST" action="includes/retrieveSub.php">
-                    <div class="input-group">
-                      <label for="email">Email</label><br>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter email address" required>
-                        <div class="input-group-append">
-                          <span class="input-group-text"><i class="bx bx-envelope"></i></span>
+                    <div class="sign_up_body">
+                        
+                        <div class="form_body">
+                            <div class="input-group">
+                                <label for="email">Email</label><br>
+
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter email address" required>
+
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="bx bx-envelope"></i></span>
+                                </div>
+                            </div>
+
+                            <div id="msg"></div>
+
+                            <div class="form-group">
+                                <input type="button" name="submit" id="btn_retrieve" class="modal-btn w-100" value="SUBMIT">
+                            </div>
+
+                            <div class="msg">
+                                <span>
+                                    <a href="index.php">Sign In &nbsp;/ </a>
+                                    <a href="sign_up.php">&nbsp; Sign Up</a>
+                                </span>
+                            </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="form-group">
-                        <input type="submit" name="submit" id="btn_submit" class="modal-btn w-100" value="SUBMIT">
-                    </div>
-
-                    <div class="msg">
-                        <span>
-                            <a href="index.php">Sign In &nbsp;/ </a>
-                            <a href="sign_up.php">&nbsp; Sign Up</a>
-                        </span>
-                    </div>
-                </form>
-            </div>
-            </div>
-            </div>
+                </div>
             </div>
         </div>
     </div>
-</body>
-    <?php require_once('includes/js-links.php') ?>
 
+    <?php require_once('includes/js-links.php') ?>
+    <script>
+      $(function(){
+
+        $('#btn_retrieve').click(function(){
+            const email = $('#email').val();
+
+            if(email == "" ){
+              var errorMsg = "<p class='alert alert-danger text-center'> Please fill out all fields!! </p>";
+                $('#msg').html(errorMsg);
+            }else{
+                $.ajax({
+                  method: "POST",
+                  url: "includes/retrieveSub.php",
+                  data: {email: email},
+                  success: function(response){
+                    var msg = ""; 
+                      if (response == 0) {
+                          msg = "<p class='alert alert-danger text-center'> Email address not found! </p>";
+                           $('#msg').html(msg);
+                      } else {
+                        window.location.href = 'newPassword.php?id='+response;
+                      }
+                  }
+                });
+              }
+            });
+        });
+    </script>
+    </body>
 </html>
